fc63fcac2fcf158986056717a837750660272a3b
sallanez <saul.alejandro19@gmail.com>
2022-10-28 00:08:03 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Hello } from "./components/hello.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <div>
+    <Hello />
+  </div>
+);
---
+++ webpack.config.dev.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  devServer: {
+    watchFiles: ["src/**/*"],
+    port: 3000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.html$/i,
+        use: { loader: "html-loader" },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
